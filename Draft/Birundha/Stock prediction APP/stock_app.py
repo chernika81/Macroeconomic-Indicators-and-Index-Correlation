5c4891a3dc6a790451bdc8d7f45dd9e87c7d1a9a
BirundhaVaratharajan <127799451+BirundhaVaratharajan@users.noreply.github.com>
2023-08-31 18:12:56 -0500

    Initial commit for stock prediction app

---
+++ Draft/Birundha/Stock prediction APP/stock_app.py
@@ -0,0 +1,48 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+import pandas as pd
+import yfinance as yf
+import streamlit as st
+
+
+
+
+#Create an app object using the Flask class. 
+app = Flask("stock predictor")
+
+
+@app.route('/')
+def home():
+    return(
+    # f"Choose one stock index name as :<br/>",
+    render_template('index input.html')
+    )
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    form = request.form
+    if request.method == 'POST':
+      #Load the trained model. (Pickle file)
+        model = pickle.load(open('model/model.pkl', 'rb'))
+
+        year = request.form['year']
+        ticker = request.form['ticker'].upper()
+    print("hello world")
+    period = year *365
+    
+    stock = yf.Ticker(ticker)
+    stocks_df = stock.history()
+    # stocks_df.head()
+
+    print(stocks_df.head())
+
+    test_data = stocks_df[['Open', 'High', 'Low' ]].tail(1)
+
+    predicted_stock_price = model.predict(test_data)
+    return render_template('result.html',   predicted_price=predicted_stock_price)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
